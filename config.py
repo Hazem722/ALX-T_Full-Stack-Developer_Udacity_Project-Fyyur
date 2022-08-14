7cbcd9c58a5707cb4d74000e118caff71ba10d0d
Hazem <hazemba684@gmail.com>
2022-08-14 15:11:25 +0200

    Completed Project Done On Udacity Virtual Environment

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+DB_HOST = os.getenv('DB_HOST', 'localhost:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', '123')
+DB_NAME = os.getenv('DB_NAME', 'fyyur')
+DB_PATH = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+
+# IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI=DB_PATH
+SQLALCHEMY_TRACK_MODIFICATIONS = False
